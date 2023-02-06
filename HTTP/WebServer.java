c59f3872edc5d6a6f01b1b79bf8cb4b2881679a0
Simone <baldelli.simone@gmail.com>
2023-02-06 19:26:03 +0100

    fix package name and exception handling

--- HTTP/WebServer.java
+++ HTTP/WebServer.java
@@ -1,43 +1,51 @@
-package webserver;
+package HTTP;
 
 import java.io.*;
 import java.net.*;
 import java.util.*;
 
 public class WebServer {
-    public static void main(String[] args) throws Exception{
-        String requestMessageLine;
-        String fileName;
-        ServerSocket listenSocket = new ServerSocket(6789);
-        Socket connectionSocket = listenSocket.accept();
-        BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-        DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-        requestMessageLine = inFromClient.readLine();
-        StringTokenizer tokenizedLine = new StringTokenizer(requestMessageLine);
-        if (tokenizedLine.nextToken().equals("GET")) {
-            fileName = tokenizedLine.nextToken();
-            if (fileName.startsWith("/") == true) {
-                fileName = fileName.substring(1);
+    public static void main(String[] args) {
+        try {
+            String requestMessageLine;
+            String fileName;
+            ServerSocket listenSocket = new ServerSocket(6789);
+            Socket connectionSocket = listenSocket.accept();
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            requestMessageLine = inFromClient.readLine();
+            StringTokenizer tokenizedLine = new StringTokenizer(requestMessageLine);
+            if (tokenizedLine.nextToken().equals("GET")) {
+                fileName = tokenizedLine.nextToken();
+                if (fileName.startsWith("/") == true) {
+                    fileName = fileName.substring(1);
+                }
+                File file = new File(fileName);
+                int numOfBytes = (int) file.length();
+                FileInputStream inFile = new FileInputStream(fileName);
+                byte[] fileInBytes = new byte[numOfBytes];
+                inFile.read(fileInBytes);
+                outToClient.writeBytes("HTTP/1.0 200 Document follows\r\n");
+                if (fileName.endsWith(".jpg")) {
+                    outToClient.writeBytes("Content-Type: image/jpeg\r\n");
+                }
+                if (fileName.endsWith(".gif")) {
+                    outToClient.writeBytes("Content-Type: image/gif\r\n");
+                }
+                outToClient.writeBytes("\r\n");
+                outToClient.write(fileInBytes, 0, numOfBytes);
+                inFile.close();
+                connectionSocket.close();
+            } else {
+                System.out.println("Bad Request Message");
             }
-            File file = new File(fileName);
-            int numOfBytes = (int) file.length();
-            FileInputStream inFile = new FileInputStream(fileName);
-            byte[] fileInBytes = new byte[numOfBytes];
-            inFile.read(fileInBytes);
-            outToClient.writeBytes("HTTP/1.0 200 Document follows\r\n");
-            if (fileName.endsWith(".jpg")) {
-                outToClient.writeBytes("Content-Type: image/jpeg\r\n");
-            }
-            if (fileName.endsWith(".gif")) {
-                outToClient.writeBytes("Content-Type: image/gif\r\n");
-            }
-            outToClient.writeBytes("\r\n");
-            outToClient.write(fileInBytes, 0, numOfBytes);
-            inFile.close();
-            connectionSocket.close();
-        } else {
-            System.out.println("Bad Request Message");
+            listenSocket.close();
+        } 
+        catch(FileNotFoundException fne){
+            System.err.println(fne.getMessage());
         }
-        listenSocket.close();
+        catch (IOException ioe) {
+            System.err.println(ioe.getMessage());
+        }
     }
 }
